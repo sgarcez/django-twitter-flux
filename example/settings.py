79b2cc8c4cf5d9c3c793f34d9007671e1b4753f4
Sergio Garcez <sergio@piratalondon.com>
2013-05-03 09:49:38 +0100

    Slimmed down example project

--- example/settings.py
+++ example/settings.py
@@ -1,9 +1,3 @@
-# Django settings for website project.
-
-import os
-# site root is the parent folder relative to this file ie the 'project/project/' dir
-SITE_ROOT = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -13,27 +7,7 @@ DATABASES = {
 
 DEBUG = True
 
-TIME_ZONE = 'Europe/London'
-
-LANGUAGE_CODE = 'en-gb'
-
-# USE_I18N = False
-
-# USE_L10N = False
-
-# USE_TZ = False
-
-MEDIA_ROOT = os.path.join(SITE_ROOT, 'media')
-
-MEDIA_URL = '/formula1/media/'
-
-STATIC_ROOT = os.path.join(SITE_ROOT, '../collected/static/')
-
-STATIC_URL = '/formula1/static/'
-
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-)
+STATIC_URL = '/static/'
 
 SECRET_KEY = 'xxx'
 
@@ -42,10 +16,7 @@ TEMPLATE_LOADERS = (
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
-    # 'django.core.context_processors.request',
     'django.contrib.auth.context_processors.auth',
-    # 'django.core.context_processors.media',
-    # 'django.core.context_processors.static',
     'django.contrib.messages.context_processors.messages',
 )
 
@@ -54,14 +25,12 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
 )
 
-ROOT_URLCONF = 'project.urls'
+ROOT_URLCONF = 'urls'
 
-WSGI_APPLICATION = 'project.wsgi.application'
+WSGI_APPLICATION = 'wsgi.application'
 
-SITE_ID = 1
 
 INSTALLED_APPS = (
     'django.contrib.auth',
